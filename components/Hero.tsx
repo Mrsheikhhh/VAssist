496e60e86eeb78ca83764d31812b7eb46b329248
Mrsheikhhh <sheikhahmed45670@gmail.com>
2024-02-13 00:01:50 +0500

    ccc

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
 
   return (
     <>
-      <section className="max-cainer padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row " style={{ backgroundColor: '#282828' }}>
+      <section className="max-cainer padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row justify-center" style={{ backgroundColor: '#282828' }}>
         <div className="hero-map" />
 
         <div className="relative z-20 flex flex-1 flex-col xl:w-1/2 ml-10" >
